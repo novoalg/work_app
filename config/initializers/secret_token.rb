1244b4d9c09972056457d2723376997d44539796
novoalg <novoalg@uwec.edu>
2014-02-19 14:38:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkApp::Application.config.secret_token = 'f6a39385cf22b3ba82a5ddcde860a34ea2e687f5c236fc4524bc456ae397e013be5eeb7094e04f506d3790719051654b488b1f99b4b416e4809f4927a36e6a52'
